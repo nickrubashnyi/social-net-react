e05092eee3204c89f13eaffc7ef4cbd474553513
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2021-03-25 20:14:55 +0200

    les 19. (Route, BrowserRouter, routing) & Adding new components

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import s from './Settings.module.css'
+
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings</h1>
+        </div>
+    )
+}
+
+export default Settings;
