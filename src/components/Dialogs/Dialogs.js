d23be6f2c56d4a67fae435ac7bd803f446adb8f5
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2021-03-25 13:57:15 +0200

    les 18. Add Dialogs component

---
+++ src/components/Dialogs/Dialogs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import s from './Dialogs.module.css'
+
+const Dialogs = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Dialogs;
