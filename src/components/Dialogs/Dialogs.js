8a6f0eacdab1f050a68ae1734d0b687cebd901c1
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2021-06-07 19:20:10 +0300

    les 21. Work with dialogs page

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -4,8 +4,19 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Nick</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Lola</div>
+                <div className={s.dialog}>Jinjer</div>
+                <div className={s.dialog}>Roma</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>I'm</div>
+                <div className={s.message}>noob</div>
+            </div>
         </div>
     )
 }
